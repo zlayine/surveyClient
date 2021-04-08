11d45b71bbfe880aebd490b3d8d9b741696f726f
Zouheir Layine <zolay1996@gmail.com>
2021-04-08 23:44:42 +0100

    fixed auth page

--- src/pages/Auth.vue
+++ src/pages/Auth.vue
@@ -1,9 +1,14 @@
 <template>
   <div
-    class="container mx-auto pt-5 px-10 mb-32 flex flex-col justify-center items-center"
+    class="container mx-auto pt-5 px-3 sm:px-10 mb-32 flex flex-col justify-center items-center"
   >
-    <div class="w-full sm:w-1/4 mx-auto">
-      <img class="w-full" :src="login_img" alt="login_img" />
+    <div class="w-full sm:w-1/4 mx-auto relative h-48">
+      <div class="absolute left-0 right-0">
+        <img class="w-full" :src="login_img" alt="login_img" />
+      </div>
+      <div class="absolute left-0 right-0 top-0 bottom-0 flex">
+        <img class="w-1/2 m-auto" :src="logo1337" alt="login_img" />
+      </div>
     </div>
     <div class="mt-7" v-if="!$route.query.code">
       <button
@@ -18,19 +23,20 @@
         </template>
         <template v-else> Sign in with intra </template>
       </button>
-      
     </div>
   </div>
 </template>
 
 <script>
 import login_img from "@/assets/login.svg";
+import logo_1337 from "@/assets/1337.svg";
 
 export default {
   data() {
     return {
       loading: false,
       login_img: login_img,
+      logo1337: logo_1337,
     };
   },
   created() {
